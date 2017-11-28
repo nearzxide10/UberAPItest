e0d5942204b27afbfb87fb4bfc0ef6b7233cf153
nearzxide10 <nearzxide10@gmail.com>
2017-11-28 11:58:50 +0800

    Signed-off-by: nearzxide10 <nearzxide10@gmail.com> test

---
+++ routes/users.js
@@ -0,0 +1,152 @@
+var express = require('express');
+var router = express.Router();
+var Uber = require('node-uber');
+var uber = new Uber({
+  client_id: 'PTERFXSNaLLgdB-anD2z4DcoYJ0CMH-G',
+  client_secret: '-oP3N4Zk29BFcgKeMDseV8n4E5VAJOxxC5dzXl6q',
+  server_token: 'y7h1gknRHxvppdkCyrHQETXHOtI1eSD21g1RW1dT',
+  redirect_uri: 'https://ginago-taxi.azurewebsites.net/users/api_callback',
+  name: 'APITEST',
+  language: 'zh-tw', // optional, defaults to en_US
+  sandbox: true, // optional, defaults to false
+  //proxy: 'PROXY URL' // optional, defaults to none
+});
+var Access_token='';
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+  
+});
+
+//產生Oauth認證的URL
+router.post('/geturl',function(req,res){
+	var authURL = uber.getAuthorizeUrl(['history', 'profile', 'request', 'places']);
+	res.send(authURL);
+});
+
+//產生Oauth認證的網頁for user
+router.get('/api/login', function(request, response) {
+	  var url = uber.getAuthorizeUrl(['history','profile', 'request', 'places']);
+	  console.log(response.code);
+	  response.redirect(url);
+});
+
+//Oauth回傳接口
+router.get('/api_callback',function(req,res){
+  console.log('api_callback in');
+ uber.authorizationAsync({authorization_code: req.query.code})
+    .spread(function(access_token, refresh_token, authorizedScopes, tokenExpiration) {
+      // store the user id and associated access_token, refresh_token, scopes and token expiration date
+      Access_token=access_token;
+      console.log('New access_token retrieved: ' + access_token);
+      console.log('... token allows access to scopes: ' + authorizedScopes);
+      console.log('... token is valid until: ' + tokenExpiration);
+      console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
+ 
+      // redirect the user back to your actual app
+      res.redirect('/test.html');
+    })
+    .error(function(err) {
+      console.error(err);
+    });
+});
+
+//取得地點資訊 看起來是取得地點車種
+router.post('/product_test',function(req,res){
+	uber.products.getAllForAddressAsync('台北市大同區承德路一段1號')
+	.then(function(ret) { 
+		console.log(ret); 
+		res.json(ret);
+		})
+	.error(function(err) { 
+		console.error(err); 
+		});
+	
+});
+
+//創建訂單
+router.post('/createrequest',function(req,res){
+		uber.requests.createAsync({
+		  "fare_id": "d30e732b8bba22c9cdc10513ee86380087cb4a6f89e37ad21ba2a39f3a1ba960",
+		  "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
+		  "start_latitude": 37.761492,
+		  "start_longitude": -122.423941,
+		  "end_latitude": 37.775393,
+		  "end_longitude": -122.417546
+		})
+	.then(function(ret) { 
+		console.log(ret);
+			res.json(ret);
+	})
+	.error(function(err) { console.error(err); });
+	
+});
+
+
+//估算車資(使用地址)
+router.get('/uberprice',function(req,res){
+	var formaddress =req.param('formaddress');
+	var toaddress=req.param('toaddress');
+	var data = {
+        
+        update_time: (new Date()).getTime()
+    };  
+	uber.estimates.getPriceForRouteByAddressAsync(formaddress,toaddress)
+  .then(function(resprice) {
+	  console.log(resprice); 
+	  data.localized_display_name=resprice.prices[0].localized_display_name;
+	  data.estimate=resprice.prices[0].estimate;
+	  
+	  res.json(data);
+	  
+	  
+	  })
+  .error(function(err) { 
+	  console.error(err);
+	  res.json(err);
+	  
+	  });
+	
+});
+
+//預估車輛到達時間秒
+router.get('/ETA',function(req,res){
+	uber.estimates.getETAForAddressAsync("台北市大同區承德路一段1號")
+	.then(function(ret) { 
+		console.log(ret); 
+		res.json(ret);
+		})
+	.error(function(err) { 
+		console.error(err);
+		});
+	
+	/* 寫法二
+	uber.estimates.getETAForAddressAsync("台北市大同區承德路一段1號",function(err,ret){
+		if(err) console.log(ret);
+		else res.json(ret);
+	});	
+	*/
+});
+
+//取得uber用戶資訊	
+router.post('/userProfile',function(req,res){
+	uber.user.getProfileAsync()
+	.then(function(ret) {
+		console.log(ret);
+		res.json(ret);
+		})
+	.error(function(err) {
+		console.error(err);
+		res.json(err);		
+		});
+});	
+
+/*
+router.delete('/removetoken',function(req,res){
+	res.json("OK");
+});
+*/
+
+
+module.exports = router;
